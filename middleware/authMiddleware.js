8c174f0869303c2daa229410400ee5860f46dd1a
maheshkarbhardev <maheshkarbhar.dev@gmail.com>
2025-07-01 11:14:38 +0530

    added authMiddleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+exports.protect = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "No Token ,Auth Denied." });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Invalid Token." });
+  }
+};
+
+exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Access Denied." });
+    }
+    next();
+  };
+};
